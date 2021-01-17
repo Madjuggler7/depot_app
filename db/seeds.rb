a17abf151013740d11c6191a41f70d85e3dda4f4
Alan Wake <szymon.witkowski701@gmail.com>
2021-01-17 23:31:09 +0100

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+# . . .
+Product.create!(title: 'Build Chatbot Interactions',
+description:
+%{<p>
+<em>Responsive, Intuitive Interfaces with Ruby</em>
+The next step in the evolution of user interfaces is here.
+Chatbots let your users interact with your service in their
+own natural language. Use free and open source tools along
+with Ruby to build creative, useful, and unexpected interactions
+for users. Take advantage of the Lita framework’s step-by-step
+implementation strategy to simplify bot development and testing.
+From novices to experts, chatbots are an area in which everyone
+can participate. Exercise your creativity by creating chatbot
+skills for communicating, information, and fun.
+</p>},
+image_url: 'dpchat.jpg',
+price: 20.00)
+# . . .
