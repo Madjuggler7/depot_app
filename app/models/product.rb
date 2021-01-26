c11ee81449b1297d84e39aa0a324e49dceedd21b
Alan Wake <szymon.witkowski701@gmail.com>
2021-01-26 10:42:07 +0100

    Validation of unique title

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
     validates :title, :description, :image_url, presence:true
     validates :price, numericality: { greater_than_or_equeal_to:0.01 }
-    
+    validates :title, uniqueness: true 
 end
