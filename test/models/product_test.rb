939ef6c34ce36736a143303244b762a930c8642b
Alan Wake <szymon.witkowski701@gmail.com>
2021-01-28 11:58:19 +0100

    Closer to finding the solution
    Changed method of testing from assert_equal to assert_includes

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,32 +1,23 @@
 require 'test_helper'
-
 class ProductTest < ActiveSupport::TestCase
-  belongs_to :user, optional:true
-
-  test "product attributes must not be empty" do
-    product = Product.new
-    assert product.invalid?
-    assert product.errors[:title].any?
-    assert product.errors[:description].any?
-    assert product.errors[:price].any?
-    assert product.errors[:image_url].any?
-    
-  end
-  test "product price must be positive" do
-    product = Product.new(title: "My Book Title",
-    description: "yyy",
-    image_url: "zzz.jpg")
-    product.price = -1
-    assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
-    product.errors[:price]
-    product.price = 0
-    assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
-    product.errors[:price]
-    product.price = 1
-    assert product.valid?
-    end
-     
+test "product attributes must not be empty" do
+  product = Product.new
+  assert product.invalid?
+  assert product.errors[:title].any?
+  assert product.errors[:description].any?
+  assert product.errors[:price].any?
+  assert product.errors[:image_url].any?
   end
 
+
+  test 'title must be present' do
+    product = Product.new(title: '')
+    product.valid?
+    assert_includes product.errors.messages[:title], "can't be blank"
+  end
+  test 'image_url must be present' do
+   product = Product.new(image_url: '')
+    product.valid?
+    assert_includes product.errors.messages[:image_url], "can't be blank"
+  end
+end
