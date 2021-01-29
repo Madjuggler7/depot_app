484bdfe5c46dc52466257c1b78218e0a242f21bb
Alan Wake <szymon.witkowski701@gmail.com>
2021-01-29 09:51:35 +0100

    Changing place of vaidations
    I moved validations from product_test to product but this time working piece of code

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,13 +1,5 @@
 require 'test_helper'
-class Product < ApplicationRecord
-  validates :title, :description, :image_url, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)\Z}i, # <<<<<----
-    message: 'must be a URL for GIF, JPG, or PNG image.'
-  }
-end
+
 
 class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
