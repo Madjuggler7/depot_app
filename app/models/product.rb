a9a413718de1d82e1a1230c4e48c3207c07e794c
Alan Wake <szymon.witkowski701@gmail.com>
2021-03-10 13:23:32 +0100

    Working with text fixtures
    Fixed issue with not passing test in yml file which are space sensitive

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,4 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image.'
     }
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-    end
+end
